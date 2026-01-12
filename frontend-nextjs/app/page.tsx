539fa18bde0555e76f7ed284f6245731403e0192
Surya Pratap Bisla <bislasuryapratap10@gmail.com>
2026-01-12 23:08:21 +0530

    add inital setup for v2 in code base

--- frontend-nextjs/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import UserHomePage from "@/components/pages/user-home"
-
-export default function HomePage() {
-  return <UserHomePage />
-}
--- frontend-nextjs/app/user/page.tsx
+++
@@ -1,6 +0,0 @@
-import UserHomePage from "@/components/pages/user-home"
-
-export default function UserPage() {
-  return <UserHomePage />
-}
-
